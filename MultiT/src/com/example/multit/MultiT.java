4a1728f5b8b05478d2542d9e27c7d0a4775f3ba6
Saurabh Kulkarni <Saurabh@Saurabhs-MacBook-Pro-3.local>
2016-01-01 14:56:39 -0800

    C3 - J

--- MultiT/src/com/example/multit/MultiT.java
+++ MultiT/src/com/example/multit/MultiT.java
@@ -3,13 +3,22 @@ package com.example.multit;
 /**
  * Created by Saurabh on 1/1/16.
  */
-public class MultiT {
+public class MultiT implements Runnable {
 
       public static void main(String args[]){
 
           System.out.println("Getting started yo.");
+          System.out.println("The main thread ID is"+ Thread.currentThread().getId());
+          MultiT worker = new MultiT();
+          Thread thread = new Thread(worker);
+          thread.start();
 
       }
 
+
+      @Override
+      public void run(){
+          System.out.println("This is the run method! The thread IS is"+Thread.currentThread().getId());
+      }
 
 }
