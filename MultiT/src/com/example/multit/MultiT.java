54ffd90c64341eb7978679b24164e579d2ae22cb
Saurabh Kulkarni <Saurabh@Saurabhs-MacBook-Pro-3.local>
2016-01-01 14:31:59 -0800

    C2 - J

---
+++ MultiT/src/com/example/multit/MultiT.java
@@ -0,0 +1,15 @@
+package com.example.multit;
+
+/**
+ * Created by Saurabh on 1/1/16.
+ */
+public class MultiT {
+
+      public static void main(String args[]){
+
+          System.out.println("Getting started yo.");
+
+      }
+
+
+}
